b8c3682648668493f189b31069b79967a346ab31
ytrofimov <Yuriy.Trofimov@aplana.com>
2023-03-09 19:08:25 +0300

    103. fix create-topics.sh

--- create-topics.sh
+++ create-topics.sh
@@ -9,4 +9,4 @@ kafka-topics --zookeeper $1:2181 --topic payment-request --create --partitions 3
 kafka-topics --zookeeper $1:2181 --topic payment-response --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper $1:2181 --topic restaurant-approval-request --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper $1:2181 --topic restaurant-approval-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic customer --create --partitions 3 --replication-factor 3 --if-not-existsexit
+kafka-topics --zookeeper $1:2181 --topic customer --create --partitions 3 --replication-factor 3 --if-not-exists
